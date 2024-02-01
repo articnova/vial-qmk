ba0a305dccaab23280645db210fb9e820850e7b1
articnova <github@articnova.io>
2024-02-01 19:15:25 +0000

    Zaq

---
+++ keyboards/articnova/zaq/config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 Michael Lau (@articnova)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
+
+#define RP2040_FLASH_GENERIC_03H
---
+++ keyboards/articnova/zaq/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD1, 0x4F, 0x26, 0x74, 0x60, 0x85, 0x8F, 0x6A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
